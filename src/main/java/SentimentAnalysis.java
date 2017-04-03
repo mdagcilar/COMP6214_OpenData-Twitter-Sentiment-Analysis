c64274d5514dd68cfcbf3d396efc1494a8308c0d
Metin Dagcilar <md3g11@soton.ac.uk>
2017-04-03 14:28:33 +0100

    added twitter4j config file for twitter API & basic search query setup

--- src/main/java/SentimentAnalysis.java
+++ src/main/java/SentimentAnalysis.java
@@ -1,5 +1,21 @@
+import twitter4j.*;
+
 /**
- * Created by Metin on 31/03/2017.
+ * SentimentAnalysis.java
  */
+
 public class SentimentAnalysis {
+
+    public static void main(String[] args){
+        Twitter twitter = TwitterFactory.getSingleton();
+        Query query = new Query("dow jones");
+        try {
+            QueryResult result = twitter.search(query);
+            for (Status status : result.getTweets()) {
+                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        }catch (TwitterException e){
+            System.out.println("Twitter query failed");
+        }
+    }
 }
